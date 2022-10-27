dfe4bfbb62317ec4bfb083c46a07d3a344a25875
onecodestar <onecodestar@outlook.com>
2022-10-27 22:33:58 +0700

    fix testing cases

--- nowwa.io/src/Core/DB/DB.ts
+++ nowwa.io/src/Core/DB/DB.ts
@@ -1,12 +1,11 @@
 import mongoose, { mongo } from 'mongoose';
 import Environment from '../Environment';
- 
+
 import { Custom, CustomProperty, CustomType, CustomDocument } from '../../Models/Custom';
 import DBMODEL from './DBMODEL';
 import LOG, { log, error } from '../UTILS/LOG';
- 
-class DB 
-{
+
+class DB {
     /*=============== 
 
 
@@ -14,31 +13,28 @@ class DB
 
 
     ================*/
- 
-    public static async init ( env:Environment.Config ):Promise<void> 
-    {
-        log( `init database...` ); 
- 
-        let uri : string = `mongodb+srv://${env.MONGODB_USER}:${env.MONGODB_PASS}@${env.MONGODB_HOST}/${env.MONGODB_DB}`;
+
+    public static async init(env: Environment.Config): Promise<void> {
+        log(`init database...`);
+
+        let uri: string = `mongodb+srv://${env.MONGODB_USER}:${env.MONGODB_PASS}@${env.MONGODB_HOST}/${env.MONGODB_DB}`;
 
         log(`connect to: ${uri}`);
 
-        await mongoose.connect( uri, 
-        {
-            ssl         : true,
-            sslValidate : false,
-            sslCert     : `${env.MONGODB_CERT}`
+        await mongoose.connect(uri,
+            {
+                ssl: true,
+                sslValidate: false,
+                sslCert: `${env.MONGODB_CERT}`
 
-        }).then(( result ) => 
-        {
-            log( "Successfully connect to MongoDB." );
+            }).then((result) => {
+                log("Successfully connect to MongoDB.");
 
-        }).catch(( e:Error ) => 
-        {
-            error( "Connection error", e );
-            throw e;
-        });
- 
+            }).catch((e: Error) => {
+                error("Connection error", e);
+                throw e;
+            });
+
         return Promise.resolve();
     }
 
@@ -50,16 +46,15 @@ class DB
 
     ================*/
 
-    public static async get( tableName:string, query:DB.Query ):Promise<CustomDocument[]> 
-    {
-        let model       = await DBMODEL.get( tableName );
-        let myQuery     = model.find( query.where || {} );
+    public static async get(tableName: string, query: DB.Query): Promise<CustomDocument[]> {
+        let model = await DBMODEL.get(tableName);
+        let myQuery = model.find(query.where || {});
 
-        if( query.limit ) myQuery.limit( query.limit );
+        if (query.limit) myQuery.limit(query.limit);
 
-        let documents   = await myQuery.exec();
-        
-        return Promise.resolve( documents );
+        let documents = await myQuery.exec();
+
+        return Promise.resolve(documents);
     }
 
     /*=============== 
@@ -69,16 +64,15 @@ class DB
     
 
     ================*/
- 
-    public static async set( tableName:string, query:DB.Query ):Promise<CustomDocument> 
-    {
-        if ( query.where ) return DB.change( tableName, query ); 
 
-        let model       = await DBMODEL.get( tableName );
-        let document    = await model.create( query.values );
-        return Promise.resolve( document );
+    public static async set(tableName: string, query: DB.Query): Promise<CustomDocument> {
+        if (query.where) return DB.change(tableName, query);
+
+        let model = await DBMODEL.get(tableName);
+        let document = await model.create(query.values);
+        return Promise.resolve(document);
     }
- 
+
 
     /*=============== 
 
@@ -87,25 +81,23 @@ class DB
     
 
     ================*/
- 
-    public static async change( tableName:string, query:DB.Query ):Promise<CustomDocument> 
-    {
-        let model           = await DBMODEL.get( tableName );
 
-        let myQuery         = model.find(query.where).limit(1);
-        let documents       = await myQuery.exec();
- 
-        if( !documents || documents.length != 1 ) return Promise.reject( new Error(`matching entry not found!`) );
-        
-        let document        = documents[0];
-        let fieldNames      = Object.keys(query.values!);
+    public static async change(tableName: string, query: DB.Query): Promise<CustomDocument> {
+        let model = await DBMODEL.get(tableName);
 
-        for( let i:number = 0; i < fieldNames.length; i++ ) 
-        {
-            let fieldName   = fieldNames[i];
-            let fieldValue  = query.values![fieldName];
+        let myQuery = model.find(query.where as any).limit(1);
+        let documents = await myQuery.exec();
 
-            document.schemaFields[fieldName] = fieldValue;
+        if (!documents || documents.length != 1) return Promise.reject(new Error(`matching entry not found!`));
+
+        let document = documents[0];
+        let fieldNames = Object.keys(query.values!);
+
+        for (let i: number = 0; i < fieldNames.length; i++) {
+            let fieldName = fieldNames[i];
+            let fieldValue = query.values![fieldName];
+
+            document[fieldName] = fieldValue;
         }
         await document.save();
         return Promise.resolve(document);
@@ -118,70 +110,73 @@ class DB
     
 
     ================*/
- 
-    public static async remove()
-    {
 
+    public static async remove(tableName: string, query: DB.Query): Promise<void> {
+        let model = await DBMODEL.get(tableName);
+
+        let myQuery = model.find(query.where as any).limit(1);
+        await model.deleteOne(myQuery);
+        return Promise.resolve();
     };
 
 
- 
-    
+
+
 }
 
-namespace DB 
-{
+namespace DB {
     export type FieldType = string | number | boolean | object | Date;
 
-    export type Fields = {[key:string]: FieldType }
+    export type Fields = { [key: string]: FieldType }
 
-    export const ReservedSchemaName:string[] = [
+    export const ReservedSchemaName: string[] = [
         'User',
         'Custom',
         'File'
     ];
-    
-    export const FieldTypeList:DB.FieldType[] = [
+
+    export const FieldTypeList: DB.FieldType[] = [
         'string',
         'number',
         'boolean',
         'object',
         'date'
     ];
- 
-    export type MapSchemaTypes = 
-    {
-        string      : string;
-        number      : number;
-        boolean     : boolean;
-        object      : object;
-        date        : Date;
-    }
 
-    export type Query = 
-    {
-        remove? : string[],
-        add?    : {[key:string]:string}
-        values? : {[key:string]:any},
-        types?  : {[key:string]:string},
-        where?  : {[key:string]:
-            string | 
-            number | 
-            boolean |
-            {
-                $lt?    : number, // less than
-                $lte?   : number, // less than equal to
-                $gt?    : number, // greater than
-                $gte?   : number, // greater than equal to
-                $ne?    : number, // not equal to
-                $in?    : number[] | string[], // in an array of
-                $nin?   : number[] | string[], // not in an array of 
-                $regex? : string | RegExp, // match regex
-                $size?  : number, // is an array with size of   
-            }
-        },
-        limit?  : number,
-    }
+    export type MapSchemaTypes =
+        {
+            string: string;
+            number: number;
+            boolean: boolean;
+            object: object;
+            date: Date;
+        }
+
+    export type Query =
+        {
+            remove?: string[],
+            add?: { [key: string]: string }
+            values?: { [key: string]: any },
+            types?: { [key: string]: string },
+            where?: {
+                [key: string]:
+                string |
+                number |
+                boolean |
+                {
+                    $lt?: number, // less than
+                    $lte?: number, // less than equal to
+                    $gt?: number, // greater than
+                    $gte?: number, // greater than equal to
+                    $ne?: number, // not equal to
+                    $in?: number[] | string[], // in an array of
+                    $nin?: number[] | string[], // not in an array of 
+                    $regex?: string | RegExp, // match regex
+                    $size?: number, // is an array with size of   
+                }
+            },
+            limit?: number,
+        }
 }
 
 export default DB;
--- nowwa.io/src/Core/TEST.ts
+++ nowwa.io/src/Core/TEST.ts
@@ -1,29 +1,71 @@
 import LOG, { log } from "./UTILS/LOG";
 import DB from "./DB/DB";
 import { randomBytes } from "crypto";
+import DBTABLE from "./DB/DBTABLE";
 
-class TEST
-{
+class TEST {
 
-    public static async test()
-    {
+    public static async test() {
         log('TESTING');
- 
-        // SAVE ITEM
 
-        var query : DB.Query = 
-        {
-            values : {
-                "userName"  : "GuestUser",
-                "password"  : "123456"
-            }
+        /* Create Dataitem */
+        let struct001 = await DBTABLE.get("schema002");
+        log(JSON.stringify(struct001));
+
+        /* Create Dataitem */
+        let input001: DB.Query = {
+            "values": {
+                "field001": 5,
+                "field002": "a string",
+                "field003": true,
+                "field004": {
+                    "key": "value"
+                }
+            },
         };
+        let item001 = await DB.set("schema002", input001);
+        let id001: string = (item001 as any)._id;
+        log(JSON.stringify(item001));
+        log('done 1');
 
-        var response = await DB.set( "USERNAME", query );
+        /* Retrieve Dataitem */
+        let input002: DB.Query = {
+            "where": {
+                "_id": id001
+            },
+        };
+        let item002 = await DB.get("schema002", input002);
+        log(JSON.stringify(item002));
+        log('done 2');
 
-        log( "Something happened", response );
+        /* Change Dataitem */
+        let input003: DB.Query = {
+            "where": {
+                "_id": id001
+            },
+            "values": {
+                "field001": 10,
+                "field002": "not a string",
+                "field003": false,
+                "field004": {
+                    "key": "value 2"
+                }
+            },
+        };
+        let item003 = await DB.set("schema002", input003);
+        log(JSON.stringify(item003));
+        log('done 3');
 
-    }  
+        /* Delete Dataitem */
+        let input004: DB.Query = {
+            "where": {
+                "_id": id001
+            },
+        };
+        await DB.remove("schema002", input004);
+        log('done 4');
+
+    }
 
 };
 
