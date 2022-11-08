76d58446eb6ca7972db4b3b0e3852d1d8affd57b
onecodestar <onecodestar@outlook.com>
2022-11-08 16:42:15 +0700

    fix session instance

--- nowwa.io/src/Frontend/User/SessionInstance.ts
+++ nowwa.io/src/Frontend/User/SessionInstance.ts
@@ -1,17 +1,15 @@
 import LOG, { log } from '../../UTIL/LOG';
 import PROMISE, { resolve } from '../../UTIL/PROMISE';
 import COS from '../ConquerOS';
- 
 
-class SessionInstance 
-{
-    private Conquer : COS;
 
-    public async init( cos:COS ) : Promise<any>
-    {
+class SessionInstance {
+    private Conquer?: COS;
+
+    public async init(cos: COS): Promise<any> {
         this.Conquer = cos;
 
-        return resolve();
+        return Promise.resolve();
     }
 };
 
