940b1107683844f050fcfebf1ea9495b1998aea3
onecodestar <onecodestar@outlook.com>
2022-12-20 14:39:36 +0700

    add gamescore sorting and filtering

--- nowwa.io/src/Core/GAME/GAMESCORE.ts
+++ nowwa.io/src/Core/GAME/GAMESCORE.ts
@@ -5,6 +5,7 @@ import FRIENDS from "../USER/TRIBE/FRIENDS/FRIENDS";
 import QUERY from "../../UTIL/QUERY";
 import AVATAR from "../USER/TRIBE/AVATAR";
 import ARRAY from "../../UTIL/ARRAY";
+import DATE from "../../UTIL/DATE";
 
 class GAMESCORE
 {
@@ -51,31 +52,45 @@ class GAMESCORE
         query       = QUERY.get( query );
         query.limit = 100;
 
+        query.sort = {
+            maxScore: 1 // 1 for ascending, -1 for descending
+        };
+
         // query.where ? maxScore > highest values, sorted down
         // TODO, get 100 results based on maxScore
 
         let values  = await this.get( query );
 
-        values      = ARRAY.sort( values, "maxScore" );
+        // values      = ARRAY.sort( values, "maxScore" );
 
-        values.reverse();
+        // values.reverse();
 
         return AVATAR.fill( values );
     };
 
     public static async getToday( query:any ) : Promise<any>
     {
+        let now = new Date(Date.now());
+        let today = new Date(now.getTime() - now.getHours()*3600000 - now.getMinutes()*60000 - now.getSeconds()*1000 - now.getMilliseconds());
         query       = QUERY.get( query );
         query.limit = 100;
+        query.where = {
+            lastChange: {
+                $gte: today
+            }
+        };
+        query.sort = {
+            score: 1 // 1 for ascending, -1 for descending
+        };
 
         // query.where ?  lastChange < DATE.now() less than one day?
         // TODO, get 100 results when lastChange happened in the last day, sorted by score
 
         let values  = await this.get( query );
 
-        values      = ARRAY.sort( values, "score" );
+        // values      = ARRAY.sort( values, "score" );
 
-        values.reverse();
+        // values.reverse();
 
         return AVATAR.fill( values );
     };
