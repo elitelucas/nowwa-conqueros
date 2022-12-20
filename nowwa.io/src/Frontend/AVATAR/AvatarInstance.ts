4cad9dce27c41cc7671be20feb1275a49b31ea76
onecodestar <onecodestar@outlook.com>
2022-12-20 15:38:58 +0100

    Conquer.Avatars

--- nowwa.io/src/Frontend/AVATAR/AvatarInstance.ts
+++ nowwa.io/src/Frontend/AVATAR/AvatarInstance.ts
@@ -9,10 +9,7 @@ class AvatarInstance
     public avatarID     : any;
     public firstName    : any;
     public userPhoto    : any;
-    public membershipID : any;
-    public status       : any;
-    public role         : any;
- 
+
     constructor( instance:CONQUER, vars:any )
     {
         this.conquer        = instance;
@@ -25,37 +22,14 @@ class AvatarInstance
         this.avatarID       = vars.avatarID || this.avatarID;
         this.firstName      = vars.firstName || this.firstName;
         this.userPhoto      = vars.userPhoto || this.userPhoto;
-
-        this.membershipID   = vars.membershipID;
-        this.status         = vars.status 
     }
 
     public async set() : Promise<any>
     {
-        let membership;
-
-        if( !this.membershipID )
-        {
-            membership      = await this.conquer.Friends.set( this.avatarID );
-
-            return Promise.resolve( this );
-        } 
- 
-        if( this.status != "pending" ) return;
-
-        membership      = await this.conquer.do( ACTIONS.FRIENDS_CHANGE, { membershipID:this.membershipID, status:"active" } );
- 
-        this.fill( membership );
-
-        return Promise.resolve( this );
+        let membership      = await this.conquer.Friends.set( this.avatarID ); 
+        return Promise.resolve( membership );
     }
 
-    public remove()
-    {
-        this.conquer.do( ACTIONS.FOLLOWERS_REMOVE, { membershipID:this.membershipID });
-        ARRAY.removeItem( this.conquer.Followers.pool, this );
-    }
- 
 }
 
 export default AvatarInstance;
