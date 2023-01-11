b57639e1b368f7de7c827d498aa0797c2982e2c0
onecodestar <onecodestar@outlook.com>
2023-01-11 23:45:24 +0900

    remove "new nowwa.io"

--- new nowwa.io/client/src/constants/whitelist.js
+++
@@ -1,5 +0,0 @@
-export const whiteList = [
-    "0x98b431D3F9a032b459201547679F3cE744114CA8",
-    "0xaF9Fd769a5FdA2b61d47fc43446407e861b996ee",
-  ];
-  
--- new nowwa.io/client/src/helper/index.js
+++
@@ -1,7 +0,0 @@
-export const shortenAddress = (address, count = 4) => {
-  return address.slice(0, count) + "..." + address.slice(-count);
-};
-
-export const getNameFromEmail = (username) => {
-  return username.slice(0, username.search("@"));
-};
