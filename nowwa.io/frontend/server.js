fa2c6c6eb76976af7714dc98b24566eb3150e1da
onecodestar <onecodestar@outlook.com>
2023-01-13 23:33:58 +0900

    adjust commands

--- nowwa.io/frontend/server.js
+++ nowwa.io/frontend/server.js
@@ -4,16 +4,13 @@ const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
-const io = socket(server);
-const username = require("username-generator");
 const path = require("path");
 // const walletController = require('./controllers/wallet');
-
 
 app.use(express.static("./build"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
 const port = process.env.PORT || 9002;
