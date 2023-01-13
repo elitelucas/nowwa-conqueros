93cf98451f50ed823f49d252329b39dd6d678d13
onecodestar <onecodestar@outlook.com>
2023-01-14 00:56:36 +0900

    remove "new nowwa.io" folder

--- new nowwa.io/client/src/entities/index.js
+++
@@ -1,2 +0,0 @@
-export { BigNumberMath } from './BigNumberMath'
-export { default as Fraction } from './Fraction'
--- new nowwa.io/client/src/screens/NAccount/XXX/index.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from "react";
-import styles from "./my.module.sass";
-import cn from "classnames";
-
-const XXX = ({ children }) => {
-  return <div className={styles.container}></div>;
-};
-
-export default XXX;
