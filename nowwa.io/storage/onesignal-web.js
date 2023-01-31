1925c1f0071fc620a7775a36133621a6e3f310a7
onecodestar <onecodestar@outlook.com>
2023-01-31 17:28:06 +0700

    add required files

---
+++ nowwa.io/storage/onesignal-web.js
@@ -0,0 +1,7 @@
+// Add to index.js or the first page that loads with your app.
+window.OneSignal = window.OneSignal || [];
+OneSignal.push(function () {
+    OneSignal.init({
+        appId: "21336dca-4458-47b3-b0e5-60fe71412024",
+    });
+});
